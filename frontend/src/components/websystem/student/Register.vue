59daf5a46141c123604b271cbf0b043ca404eca7
Jose Justin <joselsjustin@gmail.com>
2020-12-16 09:44:05 -0300

    Frontend - Integrates student register page
    Integrates the student register page backend and frontend

--- frontend/src/components/websystem/student/Register.vue
+++ frontend/src/components/websystem/student/Register.vue
@@ -8,6 +8,7 @@
             class="mt-4"
             label="Nome"
             placeholder="Informe o nome completo"
+            v-model="name"
             :rules="nameRules"
             required></v-text-field>
         </v-row>
@@ -16,6 +17,7 @@
             class="mt-4"
             label="E-mail"
             placeholder="Informe apenas um e-mail"
+            v-model="email"
             :rules="emailRules"
             required></v-text-field>
         </v-row>
@@ -24,6 +26,7 @@
             class="mt-4"
             label="RA"
             placeholder="Informe o registro acadêmico"
+            v-model="ra"
             :rules="raRules"
             required></v-text-field>
         </v-row>
@@ -32,20 +35,30 @@
             class="mt-4"
             label="CPF"
             placeholder="Informe o número do documento"
+            v-model="cpf"
             :rules="cpfRules"
             required></v-text-field>
         </v-row>
+
+        <v-alert
+          :value="alert.show"
+          :type="alert.type">
+          {{ alert.message }}
+        </v-alert>
+
         <v-row>
           <v-spacer></v-spacer>
 
           <v-btn
             color="warning"
             class="mt-4"
+            :loading="showLoading"
             to="/websystem/students-list">Cancelar</v-btn>
 
           <v-btn
             color="success"
             class="ml-2 mt-4"
+            :loading="showLoading"
             @click="saveStudent">Cadastrar</v-btn>
         </v-row>
       </v-form>
@@ -62,9 +75,15 @@
     },
     data() {
       return {
+        name: '',
+        email: '',
+        ra: '',
+        cpf: '',
+        alert: {},
+        showLoading: false,
         nameRules: [
           name => !!name || 'Nome precisa ser informado',
-          name => !!name && name.length < 4 || 'Nome completo ser informado',
+          name => (name || '').length > 3 || 'Nome completo precisa ser informado',
         ],
         emailRules: [
           email => !!email || 'E-mail precisa ser informado',
@@ -77,14 +96,71 @@
           ra => !!ra || 'RA precisa ser informado'
         ],
         cpfRules: [
-          cpf => !!cpf || 'CPF precisa ser informado'
+          cpf => !!cpf || 'CPF precisa ser informado',
+          cpf => this.checksCPF(cpf) || 'É necessário um CPF válido'
         ]
       }
     },
     methods: {
+      checksCPF(inputCPF) {
+        if(inputCPF != undefined){
+          var soma = 0;
+          var i;
+          var resto;
+          var cpf = inputCPF;
+
+          if (cpf == "00000000000") return false;
+
+          for (i = 1; i <= 9; i++)
+            soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+            resto = (soma * 10) % 11;
+
+          if (resto == 10 || resto == 11) resto = 0;
+
+          if (resto != parseInt(cpf.substring(9, 10))) return false;
+
+          soma = 0;
+          for (i = 1; i <= 10; i++)
+            soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+            resto = (soma * 10) % 11;
+
+          if (resto == 10 || resto == 11) resto = 0;
+
+          if (resto != parseInt(cpf.substring(10, 11))) return false;
+          return true;
+        }
+      },
       saveStudent() {
         if(this.$refs.form.validate()){
-          // POST
+          this.showLoading = !this.showLoading
+
+          this.$http.post(
+            '/students',
+            {
+              name: this.name,
+              email: this.email,
+              ra: this.ra,
+              cpf: this.cpf
+            },
+            {
+              headers: {
+                'Authorization': this.$store.getters['user/getToken']
+              }
+            }
+          ).then(() => {
+            this.alert.show = true;
+            this.alert.type = 'success'
+            this.alert.message = 'Estudante cadastrado com sucesso'
+            setTimeout(() => { this.alert.show = false }, 3000)
+            this.showLoading = !this.showLoading
+            this.$refs.form.reset()
+          }).catch((error) => {
+            this.alert.show = true;
+            this.alert.type = 'error'
+            this.alert.message = error.message
+            setTimeout(() => { this.alert.show = false }, 3000)
+            this.showLoading = !this.showLoading
+          });
         }
       }
     }
